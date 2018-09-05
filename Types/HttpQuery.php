87ab21d74829bcb51098c9818665623df629c489
Nils Baltuttis <nb@os-cillation.de>
2018-09-05 16:17:09 +0200

    Also Clear single quotes

--- Types/HttpQuery.php
+++ Types/HttpQuery.php
@@ -70,7 +70,7 @@ class HttpQuery {
 
         // Get WHERE conditions as string including table alias and primary key column if present
         $sqlWhereString = array_reduce($tokens['WHERE'], function($query, $token) use ($tableAlias) {
-            return $query . str_replace('"', '', str_replace('OR', '|', str_replace('AND', '&', $token['base_expr'])));
+            return $query . str_replace(['"', '\''], '', str_replace('OR', '|', str_replace('AND', '&', $token['base_expr'])));
         });
 
         // Remove primary key column before removing table alias and returning
